b8b2d2ae4beb5c212b34df33602a32be8805e41b
Sarah J Lee <44482222+sjunel@users.noreply.github.com>
2022-10-14 12:35:01 -0700

    Add static file setup

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ app.engine('handlebars', engine({}));
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.render('home');
 });
